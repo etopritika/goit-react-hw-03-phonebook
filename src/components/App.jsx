a36ceea155605997cb4319a516e6bf9345df68bf
etopritika <dimaprytyka@gmail.com>
2023-05-11 13:42:43 +0300

    init

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,16 +2,27 @@ import React, { Component } from 'react';
 import ContactForm from './ContactForm';
 import Filter from './Filter';
 import Contacts from './Contacts';
+const STORAGE_KEY = 'contacts';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const savedContacts = localStorage.getItem(STORAGE_KEY);
+    const parsedContacts = JSON.parse(savedContacts);
+    if (!parsedContacts) {
+      return;
+    }
+    this.setState({ contacts: [...parsedContacts] });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { contacts } = this.state;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(contacts));
+  }
 
   formSubmitHandler = data => {
     this.setState(prevState => ({
